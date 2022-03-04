d036a3fe6357e9cf3a6c1280aafe29e0e6b03a90
Major Hoffman <13547412+heymajor@users.noreply.github.com>
2022-03-04 17:18:20 -0500

    Initial submission

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,2 +1,15 @@
 def find(search_list, value):
-    pass
+
+    for n in search_list:
+        if n == value: 
+            return search_list.index(value)
+
+    raise ValueError("value not in array")
+
+# def main():
+#     # print(find([1, 3, 4, 6, 8, 9, 11], 6)) # 3
+#     # print(find([1, 3, 4, 6, 8, 9, 11], 13)) # ValueError
+#     print(find([1, 3, 4, 6, 8, 9, 11], 11)) # 6
+
+# if __name__ == '__main__':
+#     main()
