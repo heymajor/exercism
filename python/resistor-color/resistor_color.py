afd45e5ffc7cafc075433e6aa715f23e3b5d3ab3
Major Hoffman <13547412+heymajor@users.noreply.github.com>
2022-03-04 14:31:10 -0500

    Initial submission for resistor-color exercise

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,6 +1,17 @@
-def color_code(color):
-    pass
-
+def color_code(col):
+    colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"] 
+    for color in colors: 
+        if col == color:
+            return colors.index(color)
 
 def colors():
-    pass
+    return ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"] 
+
+
+# def main():
+#     print(color_code("black")) # 0
+#     print(color_code("white")) # 9
+#     print(color_code("orange")) # 3
+
+# if __name__ == '__main__':
+#     main()
