92d10538470c7f3e2a37e4b0a8b722b6edd0ca6e
Major Hoffman <13547412+heymajor@users.noreply.github.com>
2022-03-05 17:13:27 -0500

    Submit iteration 1

--- python/pythagorean-triplet/pythagorean_triplet.py
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -1,2 +1,16 @@
-def triplets_with_sum(number):
-    pass
+def triplets_with_sum(n):
+    solutions = []
+    for a in range(1, n):
+        denom = 2 * (n - a)
+        num = (2 * a**2) + n**2 - (2 * n * a)
+        if denom > 0 and num % denom == 0:
+            c = num // denom
+            b = n - a - c
+            if b > a:
+                solutions.append((a, b, c))
+    solutions = [list(x) for x in solutions]
+    print(solutions)
+
+    return solutions
+
+# reference: https://stackoverflow.com/questions/61755082/pythagorean-triplet-with-given-sum
