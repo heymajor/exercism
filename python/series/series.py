afeec7f1ad65f4fb3616845fb49e65a4109ee071
Major Hoffman <13547412+heymajor@users.noreply.github.com>
2022-03-14 15:14:10 -0400

    Exercise submission

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,20 @@
 def slices(series, length):
-    pass
+    if length < 0: raise ValueError('slice length cannot be negative')
+    if length == 0: raise ValueError('slice length cannot be zero')
+    if len(series) == 0: raise ValueError('series cannot be empty')
+    if len(series) < length: raise ValueError('slice length cannot be greater than series length')
+
+    chunked_list = []
+    for i in range(0, len(series)):
+        if len(series[i:length + i]) == length:
+            chunked_list.append(series[i:length + i])
+        else: break
+    
+    return chunked_list
+
+
+def main():
+    print(slices("777777", 3)) 
+
+if __name__ == '__main__':
+    main()
